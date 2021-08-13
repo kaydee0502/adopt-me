0991b56ba1d8fd6ed11de0c0e2c10c7737bb6f29
kaydee0502 <kshitijdhama513@gmail.com>
2021-08-13 21:12:01 +0530

    eslint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.car),
-        React.createElement("h3", {}, props.comp),
-        React.createElement("h3", {}, props.rate)
-    ])
-}
+/* global React ReactDOM */
 
+const Pet = (props) => {  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.car),
+    React.createElement("h3", {}, props.comp),
+    React.createElement("h3", {}, props.rate),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "About me!"),
-            React.createElement(Pet, {car: "Evo", comp: "Mitsubishi", rate: 5}),
-            React.createElement(Pet, {car: "R35", comp: "Nissan", rate: 9}),
-            React.createElement(Pet, {car: "WRX", comp: "Subaru", rate: 4}),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "About me!"),
+    React.createElement(Pet, { car: "Evo", comp: "Mitsubishi", rate: 5 }),
+    React.createElement(Pet, { car: "R35", comp: "Nissan", rate: 9 }),
+    React.createElement(Pet, { car: "WRX", comp: "Subaru", rate: 4 }),
+  ]);
+};
 
-        ]
-    )
-}
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
