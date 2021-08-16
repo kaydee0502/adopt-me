907c078161c93b1bb8941ced7433fe53130db7e9
kaydee0502 <kshitijdhama513@gmail.com>
2021-08-16 14:15:02 +0530

    added interactive component

---
+++ src/Carousel.js
@@ -0,0 +1,44 @@
+
+import React, { Component } from 'react'
+
+export class Carousel extends Component {
+    state = { active: 0 }
+
+    handleIndexClick = (event) =>{
+        console.log("clicked");
+        this.setState(
+            {
+                active: +event.target.dataset.index
+            }
+        )
+    }
+
+
+    static defaultProps = {
+        images: 'http://pets-images.dev-apis.com/pets/none.jpg'    }
+
+    render() {
+        const { active } = this.state;
+        const { images } = this.props;
+        return (
+            <div className = "carousel">
+                <img src = {images[active]} alt = "pet" />
+                <div className = "carousel-smaller">
+                {images.map((photo, index) => (
+            // eslint-disable-next-line
+                    <img
+                    key={photo}
+                    src={photo}
+                    data-index={index}
+                    onClick={this.handleIndexClick}
+                    className={index === active ? "active" : ""}
+                    alt="animal thumbnail"
+                    />
+                ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Carousel
--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,13 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
-
+import Carousel from "./Carousel";
 
 class Details extends Component {
-    constructor() {
-        super();
-        this.state = {loading: true}
-    }
 
+    state = {loading: true}
+
+  
     async componentDidMount(){
         const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`)
         const json = await res.json()
@@ -20,13 +19,14 @@ class Details extends Component {
     render() {
         console.log(this.state);
         if (this.state.loading){
-            return <h1>loading..</h1>
+            return <p>loading..</p>
         }
-        const { animal, breed, city, state, description, name } = this.state;
+        const { animal, breed, city, state, description, name, images } = this.state;
 
         return (
         <div className="details">
             <div>
+            <Carousel images={images} />;
             <h1>{name}</h1>
             <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
             <button>Adopt {name}</button>
